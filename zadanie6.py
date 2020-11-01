aa71c70d63c29f64ae48d8dc1decba7d1c44572c
01010101-coder <58442509+01010101-coder@users.noreply.github.com>
2020-11-01 19:40:29 +0300

    Задание 1-8 http://pythontutor.ru/lessons/inout_and_arithmetic_operations/

---
+++ zadanie6.py
@@ -0,0 +1,8 @@
+# Напишите программу,
+# которая считывает целое число и выводит текст,
+# аналогичный приведенному в примере (пробелы важны!).
+
+n = int(input())
+
+print('The next number for the number', n, 'is', n+1)
+print('The previous number for the number', n, 'is', n-1)
